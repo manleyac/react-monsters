bdb543b06e11f53394b6d48dd9b496b9400906db
Andrew <8879789+manleyac@users.noreply.github.com>
2019-09-11 20:48:54 -0400

    Updates

---
+++ precache-manifest.6683212ad45d8dc95e46e25fb7a320fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95e7618ebb770eff2c6e64f9b32cecb",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "b7513df0972ee2e9e5c1",
+    "url": "/react-monsters/static/css/main.d01fa4f7.chunk.css"
+  },
+  {
+    "revision": "e260c628452a8e3fa795",
+    "url": "/react-monsters/static/js/2.cdcd8c14.chunk.js"
+  },
+  {
+    "revision": "b7513df0972ee2e9e5c1",
+    "url": "/react-monsters/static/js/main.77ebe553.chunk.js"
+  },
+  {
+    "revision": "5687f1d1d38110d81090",
+    "url": "/react-monsters/static/js/runtime~main.15df8e60.js"
+  }
+]);
